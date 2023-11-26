7ef4462c00f3383b9d83f9dbc9009bb07c61ab42
kenny_robert <skenrobert@gmail.com>
2023-11-26 18:21:34 +0100

    update

--- my-app/app/index.js
+++ my-app/app/index.js
@@ -1,4 +1,8 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from '../constants';
 
 const Home = () => {
     return(
