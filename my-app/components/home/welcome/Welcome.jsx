32b64379ccb9e606d7246cd1a736c54ccbcfa2a0
kenny_robert <skenrobert@gmail.com>
2023-11-27 17:54:12 +0100

    update

--- my-app/components/home/welcome/Welcome.jsx
+++ my-app/components/home/welcome/Welcome.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
 
-import styles from './welcome.style'
+import { useRouter } from "expo-router";
+
+import styles from "./welcome.style";
 
 const Welcome = () => {
   return (
     <View>
       <Text>Welcome</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
