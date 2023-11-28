e92024a289637a23284f6db2f19c1369a6214cbb
kenny_robert <skenrobert@gmail.com>
2023-11-28 20:30:20 +0100

    update

--- my-app/components/home/popular/Popularjobs.jsx
+++ my-app/components/home/popular/Popularjobs.jsx
@@ -10,20 +10,32 @@ import {
 import { useRouter } from "expo-router";
 
 import styles from "./popularjobs.style";
-import { COLORS, SIZES } from '../../../constants'
+import { COLORS, SIZES } from "../../../constants";
 import { PopularJobCard } from "../../common/cards/popular/PopularJobCard";
 
 const Popularjobs = () => {
   const router = useRouter();
+  const isLoading = false;
+  const error = false;
 
   return (
     <View style={styles.container}>
-        <View style={styles.header}>
-            <Text style={styles.headerTitle} >Popular jobs</Text>
-            <TouchableOpacity>
-              <Text style={styles.headerBtn}>Show all</Text>
-            </TouchableOpacity>
-        </View>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text> Something went wrong </Text>
+        ) : (
+          <FlatList />
+        )}
+      </View>
     </View>
   );
 };
