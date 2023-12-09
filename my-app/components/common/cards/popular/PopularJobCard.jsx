902e37e19194038f021fd697c6c67634142ce2e7
kenny_robert <skenrobert@gmail.com>
2023-12-09 21:28:04 +0100

    update

--- my-app/components/common/cards/popular/PopularJobCard.jsx
+++ my-app/components/common/cards/popular/PopularJobCard.jsx
@@ -26,8 +26,12 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
       <Text style={styles.companyName} numberOfLines={1}>
         {item.employer_name}
       </Text>
-
-      
+      <View style={styles.infoContainer}>
+      <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item.job_title}
+        </Text>
+        
+      </View> 
       {/* 
 
       <View style={styles.infoContainer}>
