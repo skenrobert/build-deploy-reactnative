52b2cbfb45e1d884b5dd08b2c4d75961ff3334e4
kenny_robert <skenrobert@gmail.com>
2023-12-11 21:20:21 +0100

    update

--- my-app/components/common/cards/nearby/NearbyJobCard.jsx
+++ my-app/components/common/cards/nearby/NearbyJobCard.jsx
@@ -13,7 +13,7 @@ const NearbyJobCard = ({ job, handleNavigate }) => {
               ? job.employer_logo
               : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
           }}
-          resizeMode='contain'
+          resizeMode="contain"
           style={styles.logImage}
         />
       </TouchableOpacity>
