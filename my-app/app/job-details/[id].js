a4296238e0b14f0c6bb4b7d2477630e1b37c7ef6
kenny_robert <skenrobert@gmail.com>
2023-12-12 20:13:08 +0100

    update

--- my-app/app/job-details/[id].js
+++ my-app/app/job-details/[id].js
@@ -1,4 +1,26 @@
-import { Text } from 'react-native'
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from "../../components";
+import { COLORS, icons, SIZES } from "../../constants";
+import useFetch from "../../hook/useFetch";
+
+const tabs = ["About", "Qualifications", "Responsibilities"];
 
 const JobDetails = () => {
   return (
