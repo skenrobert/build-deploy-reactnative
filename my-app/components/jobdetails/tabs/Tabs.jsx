e6876c80bffcc869639496749579adfbbc7e87ee
kenny_robert <skenrobert@gmail.com>
2023-12-13 21:23:53 +0100

    update

--- my-app/components/jobdetails/tabs/Tabs.jsx
+++ my-app/components/jobdetails/tabs/Tabs.jsx
@@ -15,12 +15,25 @@ function TabButton({ name, activeTab, onHandleSearchType }) {
   );
 } 
 
-const Tabs = () => {
+const Tabs = ({ tabs, activeTab, setActiveTab }) => {
   return (
-    <View>
-      <Text>Tabs</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={tabs}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <TabButton
+            name={item}
+            activeTab={activeTab}
+            onHandleSearchType={() => setActiveTab(item)}
+          />
+        )}
+        contentContainerStyle={{ columnGap: SIZES.small / 2 }}
+        keyExtractor={(item) => item}
+      />
     </View>
-  )
-}
+  );
+};
 
 export default Tabs
