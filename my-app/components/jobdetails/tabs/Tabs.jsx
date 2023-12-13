31e6f0a4de6d68b14c05ec5789690b0092484695
kenny_robert <skenrobert@gmail.com>
2023-12-13 21:23:22 +0100

    update

--- my-app/components/jobdetails/tabs/Tabs.jsx
+++ my-app/components/jobdetails/tabs/Tabs.jsx
@@ -4,6 +4,17 @@ import { TouchableOpacity, FlatList, Text, View } from "react-native";
 import styles from "./tabs.style";
 import { SIZES } from "../../../constants";
 
+function TabButton({ name, activeTab, onHandleSearchType }) {
+  return (
+    <TouchableOpacity
+      style={styles.btn(name, activeTab)}
+      onPress={onHandleSearchType}
+    >
+      <Text style={styles.btnText(name, activeTab)}>{name}</Text>
+    </TouchableOpacity>
+  );
+} 
+
 const Tabs = () => {
   return (
     <View>
