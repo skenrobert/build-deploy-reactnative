5ca45f647c2b9fdd64d5220f5181829822472b30
kenny_robert <skenrobert@gmail.com>
2023-12-14 19:43:10 +0100

    update

--- my-app/components/jobdetails/about/About.jsx
+++ my-app/components/jobdetails/about/About.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text } from "react-native";
 
-import styles from './about.style'
+import styles from "./about.style";
 
 const About = () => {
   return (
