7043726c29ddcbca2eb9ba48d50f5a9e73a3b02b
kenny_robert <skenrobert@gmail.com>
2023-12-14 22:18:20 +0100

    update

--- my-app/components/jobdetails/footer/Footer.jsx
+++ my-app/components/jobdetails/footer/Footer.jsx
@@ -3,7 +3,7 @@ import { View, Text, TouchableOpacity, Image, Linking } from "react-native";
 import styles from "./footer.style";
 import { icons } from "../../../constants";
 
-const Footer = () => {
+const Footer = ({ url }) => {
   return (
     <View>
       <Text>Footer</Text>
