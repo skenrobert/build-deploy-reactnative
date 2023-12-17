804cdab537909a1d70ff6f64a380d4e63f6a952a
kenny_robert <skenrobert@gmail.com>
2023-12-17 20:57:19 +0100

    update

--- my-app/app/search/[id].js
+++ my-app/app/search/[id].js
@@ -8,10 +8,19 @@ import { ScreenHeaderBtn, NearbyJobCard } from '../../components'
 import { COLORS, icons, SIZES } from '../../constants'
 import styles from '../../styles/search'
 
-const [id] = () => {
+const JobSearch = () => {
+
+  const params = useSearchParams();
+  const router = useRouter()
+
+  const [searchResult, setSearchResult] = useState([]);
+  const [searchLoader, setSearchLoader] = useState(false);
+  const [searchError, setSearchError] = useState(null);
+  const [page, setPage] = useState(1); 
+  
   return (
     <div>[id]</div>
   )
 }
 
-export default [id]
+export default JobSearch;
