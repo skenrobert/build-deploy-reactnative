b82fea671f062f52bc51bf90812b585a44b15ac6
kenny_robert <skenrobert@gmail.com>
2023-12-17 20:55:43 +0100

    update

--- my-app/app/search/[id].js
+++ my-app/app/search/[id].js
@@ -1,4 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { ActivityIndicator, FlatList, Image, TouchableOpacity, View } from 'react-native'
+import { Stack, useRouter, useSearchParams } from 'expo-router'
+import { Text, SafeAreaView } from 'react-native'
+import axios from 'axios'
+
+import { ScreenHeaderBtn, NearbyJobCard } from '../../components'
+import { COLORS, icons, SIZES } from '../../constants'
+import styles from '../../styles/search'
 
 const [id] = () => {
   return (
