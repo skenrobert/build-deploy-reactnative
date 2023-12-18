91a24c7b6cd980d5063b8dd54c44948d222dca3a
kenny_robert <skenrobert@gmail.com>
2023-12-18 18:40:11 +0100

    update

--- my-app/app/search/[id].js
+++ my-app/app/search/[id].js
@@ -27,7 +27,7 @@ const JobSearch = () => {
             method: "GET",
             url: `https://jsearch.p.rapidapi.com/search`,
             headers: {
-                "X-RapidAPI-Key": '',
+                "X-RapidAPI-Key": '3fc3d35965mshbd6ba9e36eec150p118711jsna0dbc7f26866',
                 "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
             },
             params: {
@@ -61,9 +61,78 @@ useEffect(() => {
 }, [])
 
 
-  return (
-    <div>[id]</div>
-  )
+return (
+  <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+          options={{
+              headerStyle: { backgroundColor: COLORS.lightWhite },
+              headerShadowVisible: false,
+              headerLeft: () => (
+                  <ScreenHeaderBtn
+                      iconUrl={icons.left}
+                      dimension='60%'
+                      handlePress={() => router.back()}
+                  />
+              ),
+              headerTitle: "",
+          }}
+      />
+
+      <FlatList
+          data={searchResult}
+          renderItem={({ item }) => (
+              <NearbyJobCard
+                  job={item}
+                  handleNavigate={() => router.push(`/job-details/${item.job_id}`)}
+              />
+          )}
+          keyExtractor={(item) => item.job_id}
+          contentContainerStyle={{ padding: SIZES.medium, rowGap: SIZES.medium }}
+          ListHeaderComponent={() => (
+              <>
+                  <View style={styles.container}>
+                      <Text style={styles.searchTitle}>{params.id}</Text>
+                      <Text style={styles.noOfSearchedJobs}>Job Opportunities</Text>
+                  </View>
+                  <View style={styles.loaderContainer}>
+                      {searchLoader ? (
+                          <ActivityIndicator size='large' color={COLORS.primary} />
+                      ) : searchError && (
+                          <Text>Oops something went wrong</Text>
+                      )}
+                  </View>
+              </>
+          )}
+          ListFooterComponent={() => (
+              <View style={styles.footerContainer}>
+                  <TouchableOpacity
+                      style={styles.paginationButton}
+                      onPress={() => handlePagination('left')}
+                  >
+                      <Image
+                          source={icons.chevronLeft}
+                          style={styles.paginationImage}
+                          resizeMode="contain"
+                      />
+                  </TouchableOpacity>
+                  <View style={styles.paginationTextBox}>
+                      <Text style={styles.paginationText}>{page}</Text>
+                  </View>
+                  <TouchableOpacity
+                      style={styles.paginationButton}
+                      onPress={() => handlePagination('right')}
+                  >
+                      <Image
+                          source={icons.chevronRight}
+                          style={styles.paginationImage}
+                          resizeMode="contain"
+                      />
+                  </TouchableOpacity>
+              </View>
+          )}
+      />
+  </SafeAreaView>
+)
 }
 
 export default JobSearch;
